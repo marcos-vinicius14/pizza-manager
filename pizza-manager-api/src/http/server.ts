0a561b0dab023f14810552b0d3d9b2d02caf11d8
marcos <marcos.vinicius.dev3@gmail.com>
2025-10-26 10:23:40 -0300

    feat: finish setup jwt token and cookie

--- pizza-manager-api/src/http/server.ts
+++ pizza-manager-api/src/http/server.ts
@@ -1,42 +1,25 @@
 import logger from "../../logger";
 import chalk from 'chalk'
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { appRoutes } from "./routes";
+import jwt from "@elysiajs/jwt";
+import cookie from "@elysiajs/cookie";
 
 const PORT = process.env.PORT || 3333;
 const ENV = process.env.NODE_ENV || 'development';
 
 const app = new Elysia()
   .use(appRoutes)
+  .use(jwt({
+    secret: process.env.JWT_SECRET_KEY!,
+    schema: t.Object({
+      sub: t.String(),
+      restaurantId: t.Optional(t.String()),
 
-app.onError(({ code, error, set }) => {
-  logger.error(chalk.redBright(`✗ Error: ${error}, with code: ${code}`));
+    })
+  }))
+  .use(cookie());
 
-  if (code === 'VALIDATION') {
-    set.status = 400;
-    return {
-      success: false,
-      error: 'Dados inválidos',
-      details: error.message
-    };
-  }
-
-  if (code === 'NOT_FOUND') {
-    set.status = 404;
-    return {
-      success: false,
-      error: 'Recurso não encontrado'
-    };
-  }
-
-  set.status = 500;
-  return {
-    success: false,
-    error: ENV === 'production'
-      ? 'Erro interno do servidor'
-      : error
-  };
-});
 
 app.listen(PORT, () => {
   logger.info(chalk.greenBright(`✓ Server running`));
